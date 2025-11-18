ac9ac74483f65a6266cc762013a2a881ed7c82cb
Danil0QP <daniloponciano494@gmail.com>
2025-11-17 22:38:44 -0300

    Updated Service package momentarily

--- src/main/java/CardosoLavacao/Service/ClienteService.java
+++ src/main/java/CardosoLavacao/Service/ClienteService.java
@@ -1,18 +1,22 @@
 package CardosoLavacao.Service;
 
 import CardosoLavacao.dto.ClienteDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClienteService {
 
     public ClienteDTO criarUsuario(ClienteDTO clienteDTO) {
+        return clienteDTO;
     }
 
     public ClienteDTO atualizarCliente(Long id, ClienteDTO clienteDTO) {
+        return clienteDTO;
     }
 
     public ClienteDTO findClienteById(Long id) {
+        return null;
     }
 
     public void apagarCliente(Long id) {
